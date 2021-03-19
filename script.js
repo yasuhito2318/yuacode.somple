bd6703ccba6bf5c2745908182d8ad624a34290f5
yasuhito2318 <78135620+yasuhito2318@users.noreply.github.com>
2021-03-19 15:48:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -79,10 +79,28 @@ $(function(){
         }else if ($('#check-box').prop("checked")==false) {
             alert('同意されていません。');
             return false;
-        }else {
-            alert('受け取りました。');
-            return false;
         }
+    });
+
+    $(document).ready(function () {
+
+        $('#form').submit(function (event) {
+            var formData = $('#form').serialize();
+            $.ajax({
+                url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScwosZEWG1k_uRqDWqEZAHoY3PP9dN5EPGJNil1VRfpGQkeaw/formResponse",
+                data: formData,
+                type: "POST",
+                dataType: "xml",
+                statusCode: {
+                    0: function () {
+                    $(".end-message").slideDown();
+                    $("#submit").fadeOut();
+                    },
+                }
+            });
+            event.preventDefault();
+        });
+
     });
 
     $('.fas').click(function() {
